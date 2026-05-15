1ae0bcf499d2699fc61c124bdfcdd86ac5d5a192
Igor Sampaio <isampaiodequeiroz@gmail.com>
2026-05-15 14:23:50 -0300

    Criação do login

--- src/models/LoginModel.js
+++ src/models/LoginModel.js
@@ -16,6 +16,23 @@ class Login {
         this.user = null;
     }
 
+    async login() {
+        this.valida();
+        if(this.errors.length > 0) return;
+
+        this.user = await LoginModel.findOne({ email: this.body.email });
+        if(!this.user) {
+            this.errors.push('E-mail ou senha inválidos!');
+            return;
+        }
+
+        if(!bcryptjs.compareSync(this.body.senha, this.user.senha)) {
+            this.errors.push('Senha inválida!');
+            this.user = null;
+            return;
+        }
+    }
+
     async register() {
         this.valida();
         if(this.errors.length > 0) return;
@@ -27,21 +44,12 @@ class Login {
         const salt = bcryptjs.genSaltSync();
         this.body.senha = bcryptjs.hashSync(this.body.senha, salt);
         
-        try {
-        
-            this.user = await LoginModel.create(this.body);
-        } catch(e) {
-            console.log(e);
-        }
+        this.user = await LoginModel.create(this.body);
     };
 
     async userExists() {
-        try {
-            const user = await LoginModel.findOne({ email: this.body.email })
-            if(user) this.errors.push('Usuário já existe!')
-        } catch(e) {
-            console.log(e)
-        }
+        this.user = await LoginModel.findOne({ email: this.body.email });
+        if(this.user) this.errors.push('Usuário já existe!');
     }
 
     valida() {
